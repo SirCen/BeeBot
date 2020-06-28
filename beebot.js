29852c160e036573721b96d64ea60ea9883eb88e
NDRevo <durann031@gmail.com>
2020-06-27 22:10:37 -0400

    Changed mysql stuff

--- beebot.js
+++ beebot.js
@@ -1,7 +1,8 @@
 //Libraries of somethin
 const fs = require('fs')
 const Discord = require('discord.js');
-const {prefix,token} = require('./config.json');
+const { prefix, token } = require('./config.json');
+var mysql = require('mysql');
 
 //some magic shit
 const beebot = new Discord.Client();
@@ -10,57 +11,109 @@ beebot.commands = new Discord.Collection();
 //gets commands file
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
-//Error if bot cant turn on
-beebot.on( "error", (error) => {
-	console.log( "Error encountered: " + error );
-});
-   
 //Gets commands
 for (const file of commandFiles) {
-	const command = require(`./commands/${file}`);
-	beebot.commands.set(command.name, command);
+    const command = require(`./commands/${file}`);
+    beebot.commands.set(command.name, command);
 }
+
+var connection = mysql.createConnection({
+    debug: ['ComQueryPacket', 'RowDataPacket'],
+    host: 'localhost',
+    user: 'root',
+    password: 'Durann31!',
+    database: 'birthdays'
+});
+
+connection.on("error", (error) => {
+    console.log("mysql Error encountered: " + error);
+});
+
+
+connection.connect(function (err) {
+    if (err) {
+        console.error('error: ' + err.message);
+        //Trys to reconnect
+        setTimeout(() => {
+            connection.connect(function (err) {
+                if (err) {
+                    console.error('error: ' + err.message);
+                }
+            });
+        }, 120000);
+
+    }
+});
+
+
+
+beebot.on("guildCreate", guild => {
+    let createBirthdays = `CREATE TABLE IF NOT EXISTS \`${guild.id}\` ( Who varchar(255), Date varchar(255));`;
+    connection.query(createBirthdays, function (err, results, fields) {
+        if (err) console.log(err.message);
+
+    });
+
+});
+
+beebot.on("guildDelete", guild => {
+    let removeEvents = `DROP TABLES \`${guild.id}\``;
+    connection.query(removeEvents, function (err, results, fields) {
+        if (err) {
+            console.log(err.message);
+        }
+    });
+
+});
 
 //This shit ready
 beebot.on('ready', message => {
     console.log('I am ready!');
-    var channelthis = beebot.channels.get('518472414519230467');
-    channelthis.send("Noé decided to turn my ass off to change whatevers inside me, and now im a whole new bee. Beerazy")
 });
 
 //Waits for the message
-beebot.on('message',message => {
+beebot.on('message', message => {
     //Checks every message, is bee?
-    if(message.content.includes("bee") && message.content.length == 3){
+    if (message.content.includes("bee") && message.content.length == 3) {
         message.react('🐝');
     }
     //If it starts with ! -> command
-    if(!message.content.startsWith(prefix) || message.author.bot) return;
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
     let args = message.content.slice(prefix.length).split(/ +/);
     const commandName = args.shift().toLowerCase();
 
     //Wrong args because people are fucking stupid
     const command = beebot.commands.get(commandName) || beebot.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
     if (!command) return;
-    if(command.args && !args.length){
+    if (command.args && !args.length) {
         let reply = `You didn't provide any arguments, ${message.author}!`
-        if(command.usage){
+        if (command.usage) {
             reply += `\n**Usage**: \`${prefix}${command.name} ${command.usage}\``;
         }
         return message.channel.send(reply);
     }
-    if(args.length != command.length){
+    if (args.length != command.length) {
         return message.reply("Invalid Arguments");
     };
-    //Runs the command
-    try{  
-        command.execute(message, args,beebot);  
-    }catch(error){
+  
+    try {
+        connection.query(`SELECT * FROM \`${message.guild.id}\``, function (err, result, fields) {
+            if (err) {
+                //If bot was offline and someone added bot to server
+                if (err.code == 'ER_NO_SUCH_TABLE') {
+                    let createBirthdays = `CREATE TABLE IF NOT EXISTS \`${guild.id}\` ( Who varchar(255), Date varchar(255));`;
+                    connection.query(createBirthdays, function (err, results, fields) {
+                        if (err) console.log(err.code);
+                    });
+                }
+                else console.log(err);
+            }
+            command.execute(message, args, result, connection);
+        });
+    } catch (error) {
         console.error(error);
         message.reply('There was an error');
-    } 
-
+    }
 });
-
 //Logins in the bot
 beebot.login(token);
